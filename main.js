4b184389d2180fb356017c13a4e9a0d56ee6f81c
rebeccabosse <rebeccabosse@hotmail.fr>
2018-09-28 17:38:36 +0200

    page java

---
+++ main.js
@@ -0,0 +1,5 @@
+setTimeout(function (){
+  alert ('hey le site xxxvids est trop bien.Viens dessus stp please');
+   (confirm ('etes vous sur?'));
+  {open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');}
+},10000);
